75b47009e0f021e294c09fada1cdebd435fcddeb
Leetzner <ognill@hotmail.com>
2023-01-15 13:38:06 +0000

    added a read more function to posts

--- diary/urls.py
+++ diary/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from my_diary.views import frontpage
+from my_diary.views import frontpage, post_detail
 
 urlpatterns = [
     path('', frontpage, name='frontpage'),
     path('admin/', admin.site.urls),
+    path('<slug:slug>/', post_detail, name='post_detail')
 ]
--- my_diary/views.py
+++ my_diary/views.py
@@ -7,3 +7,9 @@ def frontpage(request):
     posts = Post.objects.all()
 
     return render(request, 'my_diary/frontpage.html', {'posts': posts})
+
+
+def post_detail(request, slug):
+    post = Post.objects.get(slug=slug)
+
+    return render(request, 'my_diary/post_detail.html', {'post': post})
