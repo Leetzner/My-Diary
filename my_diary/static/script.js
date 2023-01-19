e59915aca97c3bb34c72fc7750f8d0b1cf28c75f
Leetzner <ognill@hotmail.com>
2023-01-19 15:43:11 +0000

    .

--- my_diary/static/script.js
+++
@@ -1,8 +0,0 @@
-var username = document.getElementById("username");
-var password = document.getElementById("password");
-var form = document.getElementById("form");
-
-form.addEventListener("submit", function(evt) {
-    evt.preventDefault();
-    console.log(username.value)
-});
